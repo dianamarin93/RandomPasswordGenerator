c3677b079722adbb68931bb9afb967680d5438b9
dianaM <diana.marin9324@gmail.com>
2023-01-12 15:51:42 +0200

    Add project files.

---
+++ RandomPasswordGenerator/Generator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace RandomPasswordGenerator
+{
+    public static class Generator
+    {
+        private static readonly char[] chars =
+            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!#$%&'()*+`-./".ToCharArray();
+        public static string Generate(int size)
+        {
+            
+            byte[] data = new byte[size * 10];
+
+            using(RNGCryptoServiceProvider cryptoProvider = new RNGCryptoServiceProvider())
+                cryptoProvider.GetBytes(data);
+
+            StringBuilder stringBuilder = new StringBuilder(size);
+
+            for (int i = 0; i < size; i++)
+            {
+                long random = BitConverter.ToUInt32(data, i * 10) % chars.Length;
+                stringBuilder.Append(chars[random]); 
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ RandomPasswordGenerator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using TextCopy;
+
+namespace RandomPasswordGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("================== Welcome to Random Password Generator ==================");
+            Console.WriteLine("How long should be password be?");
+
+            int size = 0;
+
+            while (size <= 5)
+            {
+                int.TryParse(Console.ReadLine(), out size); 
+
+                if (size <= 5)
+                {
+                    Console.WriteLine("Please insert a numeric value higher or equal to 5");
+                }
+            }
+
+
+            string password = Generator.Generate(size);
+            ClipboardService.SetText(password);
+            Console.WriteLine("Your password has been generated and copied to clipboard");
+            Console.WriteLine("=====================================");
+            Console.WriteLine(password);
+            Console.Read();
+        }
+    }
+}
